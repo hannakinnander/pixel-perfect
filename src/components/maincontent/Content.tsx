1a076c500c8a40edf3e8f97aa17f7246eb176a9a
hannakinnander <kinnander95@live.se>
2026-06-05 09:33:22 +0200

    fixed layout with max-width for content

--- src/components/maincontent/Content.tsx
+++ src/components/maincontent/Content.tsx
@@ -4,15 +4,17 @@ import { rooms, hotels } from "../../data";
 
 const Content = () => {
   return (
-    <div className="pl-22 pr-22 pt-13 flex flex-col items-center">
-      <SectionHeading text="Populära boenden i Göteborg" />
-      <ItemSection items={rooms} />
+    <div className="w-full flex justify-center">
+      <div className="w-full max-w-360 p-12">
+        <SectionHeading text="Populära boenden i Göteborg" />
+        <ItemSection items={rooms} />
 
-      <SectionHeading text="Fantastiska hotell för din nästa resa" />
-      <p className="w-full text-zinc-500 text-[0.97em]/3 text-shadow-2xs">
-        Dessutom får du Airbnb-kredit för din vistelse på ett utvalt hotell.
-      </p>
-      <ItemSection items={hotels} />
+        <SectionHeading text="Fantastiska hotell för din nästa resa" />
+        <p className="w-full text-zinc-500 text-[0.97em]/3 text-shadow-2xs">
+          Dessutom får du Airbnb-kredit för din vistelse på ett utvalt hotell.
+        </p>
+        <ItemSection items={hotels} />
+      </div>
     </div>
   );
 };
